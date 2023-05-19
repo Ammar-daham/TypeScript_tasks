3457cdebbac079f23a505da774b56909b089501b
Ammar Daham <ammar.daham@metropolia.fi>
2023-05-19 18:34:36 +0300

    Patientor backend, step2 done

--- patientor_backend/index.ts
+++ patientor_backend/index.ts
@@ -3,7 +3,12 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+import cors from 'cors'; 
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong!');
