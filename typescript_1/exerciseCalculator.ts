f15d6e357b3113df73ed11f31bb08f62a409d5e5
Ammar Daham <ammar.daham@metropolia.fi>
2023-05-19 12:48:39 +0300

    Eslint done

--- typescript_1/exerciseCalculator.ts
+++ typescript_1/exerciseCalculator.ts
@@ -17,23 +17,23 @@ const calculateExercises = (
   exerciseHours: number[],
   target: number,
 ): Result => {
-  const periodLength: number = exerciseHours.length
-  const trainingDays: number = exerciseHours.filter((hour) => hour > 0).length
-  const average: number = exerciseHours.reduce((a, b) => a + b) / periodLength
-  const success: boolean = average >= target
+  const periodLength: number = exerciseHours.length;
+  const trainingDays: number = exerciseHours.filter((hour) => hour > 0).length;
+  const average: number = exerciseHours.reduce((a, b) => a + b) / periodLength;
+  const success: boolean = average >= target;
 
-  let rating: number
-  let ratingDescription: string
+  let rating: number;
+  let ratingDescription: string;
 
   if (target / average >= 1.5) {
-    rating = 3
-    ratingDescription = 'excellent'
+    rating = 3;
+    ratingDescription = 'excellent';
   } else if (target / average >= 1) {
-    rating = 2
-    ratingDescription = 'not too bad but could be better'
+    rating = 2;
+    ratingDescription = 'not too bad but could be better';
   } else {
-    rating = 1
-    ratingDescription = 'needs improvement'
+    rating = 1;
+    ratingDescription = 'needs improvement';
   }
 
   return {
@@ -44,31 +44,31 @@ const calculateExercises = (
     ratingDescription,
     target,
     average,
-  }
-}
+  };
+};
 
 const parseArguments = (args: string[]): inputValues_1 => {
   if (args.length < 4) {
-    throw new Error('Not enough arguments')
+    throw new Error('Not enough arguments');
   }
-  const parsedArgs = args.slice(2).map(Number)
+  const parsedArgs = args.slice(2).map(Number);
   if (parsedArgs.some(isNaN)) {
-    throw new Error('Provided values were not numbers!')
+    throw new Error('Provided values were not numbers!');
   }
   return {
     target: parsedArgs[0],
     exerciseHours: parsedArgs.slice(1),
-  }
-}
+  };
+};
 
 try {
-  const { target, exerciseHours } = parseArguments(process.argv)
-  const result: Result = calculateExercises(exerciseHours, target)
-  console.log(result)
+  const { target, exerciseHours } = parseArguments(process.argv);
+  const result: Result = calculateExercises(exerciseHours, target);
+  console.log(result);
 } catch (error) {
-  let errorMessage = 'Something bad happened.'
+  let errorMessage = 'Something bad happened.';
   if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
+    errorMessage += ' Error: ' + error.message;
   }
-  console.log(errorMessage)
+  console.log(errorMessage);
 }
--- typescript_1/index.ts
+++ typescript_1/index.ts
@@ -1,32 +1,32 @@
-import express, { Request, Response } from 'express'
-import { calculateBmi } from './bmiCalculator'
+import express, { Request, Response } from 'express';
+import { calculateBmi } from './bmiCalculator';
 
-const app = express()
-const port = 3002
+const app = express();
+const port = 3002;
 
 app.get('/hello', (_req: Request, res: Response) => {
-  res.send('Hello Full Stack!')
-})
+  res.send('Hello Full Stack!');
+});
 
 // Endpoint for BMI calculator
 app.get('/bmi', (req: Request, res: Response) => {
-  const height = Number(req.query.height)
-  const weight = Number(req.query.weight)
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
 
   if (isNaN(height) || isNaN(weight)) {
-    res.status(400).json({ error: 'malformatted parameters' })
-    return
+    res.status(400).json({ error: 'malformatted parameters' });
+    return;
   }
 
-  const bmi = calculateBmi(height, weight)
+  const bmi = calculateBmi(height, weight);
 
   res.json({
     weight,
     height,
     bmi,
-  })
-})
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
