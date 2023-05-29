738987ab8a5b0e6caaa429358b37fecceb0157f5
Ammar Daham <ammar.daham@metropolia.fi>
2023-05-29 19:39:23 +0300

    Exercise 9.18 done

--- ilari_diaries_frontend/src/App.tsx
+++ ilari_diaries_frontend/src/App.tsx
@@ -5,68 +5,91 @@ import axios from 'axios'
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([])
 
-
-  const [ date, setDate ] = useState('')
-  const [ weather, setWeather ] = useState('')
-  const [ visibility, setVisibility ] = useState('')
-  const [ comment, setComment ] = useState('')
+  const [date, setDate] = useState('')
+  const [weather, setWeather] = useState('')
+  const [visibility, setVisibility] = useState('')
+  const [comment, setComment] = useState('')
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    axios.get<DiaryEntry[]>('http://localhost:3001/api/diaries').then((response) => {
-      console.log(response.data)
-      setDiaries(response.data)
-    })
+    axios
+      .get<DiaryEntry[]>('http://localhost:3001/api/diaries')
+      .then((response) => {
+        console.log(response.data)
+        setDiaries(response.data)
+      })
   }, [])
 
-  const diaryCreation = (event: React.SyntheticEvent) => {
+  const diaryCreation = async (event: React.SyntheticEvent) => {
     event.preventDefault()
-    axios.post<DiaryEntry>('http://localhost:3001/api/diaries', { date: date, weather: weather, visibility: visibility, comment: comment })
-    .then(response => {
-      console.log('res: ',response.data)
+    try {
+      const response = await axios.post<DiaryEntry>(
+        'http://localhost:3001/api/diaries',
+        {
+          date: date,
+          weather: weather,
+          visibility: visibility,
+          comment: comment,
+        },
+      )
+
       setDiaries(diaries.concat(response.data))
-    })
-    setDate('')
-    setWeather('')
-    setVisibility('')
-    setComment('')
+
+      setDate('')
+      setWeather('')
+      setVisibility('')
+      setComment('')
+      setError('')
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data)
+      } else {
+        console.error(error);
+      }
+    }
   }
 
   return (
     <div>
       <h1>Add new diary</h1>
+      {
+        error != null &&
+          <p style={{color: 'red'}}>Error: {error}</p>
+        
+      }
       <form onSubmit={diaryCreation}>
-        Date: 
-        <input
-          value={date}
-          onChange={(event) => setDate(event.target.value)} 
-        />
-        <br/>
-        Weather: 
+        Date:
+        <input value={date} onChange={(event) => setDate(event.target.value)} />
+        <br />
+        Weather:
         <input
           value={weather}
-          onChange={(event) => setWeather(event.target.value)} 
+          onChange={(event) => setWeather(event.target.value)}
         />
-        <br/>
-        Visibility: 
+        <br />
+        Visibility:
         <input
           value={visibility}
-          onChange={(event) => setVisibility(event.target.value)} 
+          onChange={(event) => setVisibility(event.target.value)}
         />
-        <br/>
-        Comment: 
+        <br />
+        Comment:
         <input
           value={comment}
-          onChange={(event) => setComment(event.target.value)} 
+          onChange={(event) => setComment(event.target.value)}
         />
-        <button type='submit'>add</button>
+        <br/>
+        <button type="submit">add</button>
       </form>
       <h1>Diary entries</h1>
       <>
         {diaries.map((diary) => (
           <div key={diary.id}>
             <h4>{diary.date}</h4>
-            Visibility: {diary.visibility}<br/>
-            Weather: {diary.weather}<br/>
+            Visibility: {diary.visibility}
+            <br />
+            Weather: {diary.weather}
+            <br />
             Comment: {diary.comment}
           </div>
         ))}
